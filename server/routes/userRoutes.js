8ddca6348b10ec7ed0d6b64895fc90bde7dcbe67
wjdwlsgh <wjdwlsgh02142@gmail.com>
2025-10-15 19:24:25 +0900

    feat(server/users): add JWT demo auth and update endpoint\n\n- check-id/signup/login/protected/update\n- in-memory user store and token verify

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,5 +6,6 @@ router.post("/check-id", userController.checkId);
 router.post("/signup", userController.signup);
 router.post("/login", userController.login);
 router.get("/protected", userController.protected);
+router.put("/update", userController.update);
 
 module.exports = router;
